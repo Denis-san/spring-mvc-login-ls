429b7987e685e018bb19ba0d08b5aa65d725afee
Denis-san <denilson.nt.nc@gmail.com>
2022-05-14 10:22:08 -0300

    project created

---
+++ src/main/java/br/com/san/ls/SpringMvcLoginAuthenticationLsApplication.java
@@ -0,0 +1,13 @@
+package br.com.san.ls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcLoginAuthenticationLsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcLoginAuthenticationLsApplication.class, args);
+	}
+
+}
