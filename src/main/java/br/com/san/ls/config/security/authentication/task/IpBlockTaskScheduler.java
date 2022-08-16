c1572ee4c4879d02662c8973dc00079fbd517179
Denis-san <denilson.nt.nc@gmail.com>
2022-08-16 11:29:13 -0300

    update task to clear file

--- src/main/java/br/com/san/ls/config/security/authentication/task/IpBlockTaskScheduler.java
+++ src/main/java/br/com/san/ls/config/security/authentication/task/IpBlockTaskScheduler.java
@@ -1,6 +1,9 @@
 package br.com.san.ls.config.security.authentication.task;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,11 +14,19 @@ import br.com.san.ls.config.security.authentication.constant.FilePathConstant;
 @Component
 @EnableScheduling
 public class IpBlockTaskScheduler {
-	
+
 	@Scheduled(cron = "0 59 23 ? * *", zone = "America/Sao_Paulo")
 	public void clearFile() {
 		File file = new File(FilePathConstant.PATH_FILE_ID_BLOCK);
-		file.delete();
+		try (FileWriter fileWriter = new FileWriter(file, false);
+				BufferedWriter writer = new BufferedWriter(fileWriter)) {
+
+			writer.write("");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
